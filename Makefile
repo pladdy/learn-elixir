3db8c936801613ea6b1ffb1b4ddd52217dafdf72
Matt Pladna <pladdypants@gmail.com>
2023-10-24 16:30:28 -0400

    Add Bucket module

---
+++ Makefile
@@ -0,0 +1,15 @@
+# TODO: can use 'mix' instead of 'make' for most of these.
+clean:
+	mix clean
+
+compile:
+	mix compile
+
+format:
+	mix format
+
+iex:
+	iex -S mix
+
+test:
+	mix test
---
+++ lib/kv/bucket.ex
@@ -0,0 +1,38 @@
+defmodule KV.Bucket do
+  use Agent
+
+  @doc """
+  Create a new bucket.
+  """
+  @spec new(list) :: {:ok, pid}
+  def new(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+  Delete value from bucket.
+
+  Returns the value.
+  """
+  @spec delete(pid, any) :: any
+  def delete(bucket, key) do
+    # Agent.get_and_update(bucket, &Map.pop(&1, key))
+    Agent.get_and_update(bucket, fn dict -> Map.pop(dict, key) end)
+  end
+
+  @doc """
+  Get value from a bucket.
+  """
+  @spec get(pid, any) :: any
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+  Puts the 'value' for the given 'key' in the bucket.
+  """
+  @spec put(pid, any, any) :: :ok
+  def put(bucket, key, value) do
+    Agent.update(bucket, &Map.put(&1, key, value))
+  end
+end
