1d4964a3b73f2d6053487f3ff636dde3e7d5bd21
Matt Pladna <pladdypants@gmail.com>
2023-10-30 23:35:36 -0400

    Add server

--- apps/server/lib/kv_server.ex
+++ apps/server/lib/kv_server.ex
@@ -1,18 +1,44 @@
 defmodule KVServer do
-  @moduledoc """
-  Documentation for `KVServer`.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  def accept(port) do
+    {:ok, socket} =
+      :gen_tcp.listen(
+        port,
+        [:binary, packet: :line, active: false, reuseaddr: false]
+      )
 
-  ## Examples
+    Logger.info("Accepting connection on port #{port}")
+    loop_acceptor(socket)
+  end
 
-      iex> KVServer.hello()
-      :world
+  def loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
 
-  """
-  def hello do
-    :world
+    {:ok, pid} =
+      Task.Supervisor.start_child(
+        KVServer.TaskSupervisor,
+        fn -> serve(client) end
+      )
+
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
+
+  def serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
   end
 end
--- apps/server/lib/kv_server/application.ex
+++ apps/server/lib/kv_server/application.ex
@@ -8,12 +8,13 @@ defmodule KVServer.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg)
-      # {KVServer.Worker, arg}
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec(
+        {Task, fn -> KVServer.accept(4040) end},
+        restart: :permanent
+      )
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
