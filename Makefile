47e9427f39a71c6560a9be058f991750b91f8d7f
Matt Pladna <pladdypants@gmail.com>
2023-11-06 11:29:58 -0500

    Add server commands

--- Makefile
+++ Makefile
@@ -8,8 +8,14 @@ compile:
 format:
 	mix format
 
+# Starts server
 iex:
 	iex -S mix
 
+run-client:
+	nc -v 127.0.0.1 4040
+
+# When testing, kill any iex session; with a session active port 4040 will be
+# in use and cause {:error, :eaddrinuse} from socket
 test:
 	mix test
--- apps/server/test/kv_server_test.exs
+++ apps/server/test/kv_server_test.exs
@@ -1,8 +1,52 @@
 defmodule KVServerTest do
   use ExUnit.Case
+  @moduletag :capture_log
+
   doctest KVServer
 
-  test "greets the world" do
-    assert KVServer.hello() == :world
+  setup do
+    Application.stop(:kv)
+    :ok = Application.start(:kv)
   end
+
+  setup do
+    opts = [:binary, packet: :line, active: false]
+    {:ok, socket} = :gen_tcp.connect(~c"localhost", 4040, opts)
+    %{socket: socket}
+  end
+
+  test "server interaction", %{socket: socket} do
+    # without \r\n in the command, the server timesout
+    assert send_and_recv(socket, "UNKNOWN shopping\r\n") ==
+             "Unknown command\r\n"
+
+    assert send_and_recv(socket, "GET shopping spinach\r\n") ==
+             "Not found\r\n"
+
+    assert send_and_recv(socket, "CREATE shopping\r\n") ==
+             "OK\r\n"
+
+    assert send_and_recv(socket, "PUT shopping spinach 3\r\n") ==
+             "OK\r\n"
+
+    assert send_and_recv(socket, "GET shopping spinach\r\n") == "3\r\n"
+    assert send_and_recv(socket, "") == "OK\r\n"
+
+    assert send_and_recv(socket, "DELETE shopping spinach\r\n") ==
+             "OK\r\n"
+
+    assert send_and_recv(socket, "GET shopping spinach\r\n") == "\r\n"
+    assert send_and_recv(socket, "") == "OK\r\n"
+  end
+
+  defp send_and_recv(socket, command) do
+    :ok = :gen_tcp.send(socket, command)
+    {:ok, data} = :gen_tcp.recv(socket, 0, 1000)
+    data
+  end
+end
+
+defmodule KVServer.CommandTest do
+  use ExUnit.Case, async: true
+  doctest KVServer.Command
 end
